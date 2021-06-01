a4b671230302896fd7593ed1463361f04687080a
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-01 23:24:36 +0530

    Calculated daily wage of an employee

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -7,15 +7,22 @@ public class EmpWageBuilder
 	public static void main(String[] args) 
 	{
 		int EMP_IS_FULLTIME = 1;
+		int EMP_WAGE_PER_HR = 20;
+		int empHr = 0;
+		int empWage = 0;
 		Random random = new Random();
 		int empCheck = random.nextInt(2);
 		if (EMP_IS_FULLTIME == empCheck)
 		{
+			empHr = 8;
 			System.out.println("Employee is present.");
 		}
 		else
 		{
+			empHr = 0;
 			System.out.println("Employee is absent");
 		}
+		empWage = EMP_WAGE_PER_HR * empHr;
+		System.out.println("Employee daily wage is : " + empWage + " Rs");
 	}
 }
