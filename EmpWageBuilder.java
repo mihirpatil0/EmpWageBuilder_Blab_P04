8fe165f878fdbddc8a556ee6f32f003078d6aa98
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-01 23:06:17 +0530

    Display: Welcome message

---
+++ EmpWageBuilder.java
@@ -0,0 +1,9 @@
+package empwagebuilder;
+
+public class EmpWageBuilder 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Employee Wage Computation Program.");
+	}
+}
