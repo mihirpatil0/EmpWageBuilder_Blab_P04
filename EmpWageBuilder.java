3352e3a6a1421146188681c719261146dc06716c
mihirpatil0 <mihirrpatil@gmail.com>
2021-06-01 23:58:49 +0530

    Calculated emp Monthly wage and displayed it

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,27 +3,41 @@ package empwagebuilder;
 import java.util.Random;
 
 public class EmpWageBuilder 
-{
+	{	
+		public static final int EMP_IS_FULLTIME = 2;
+		public static final int EMP_IS_PARTTIME = 1;
+		public static final int EMP_WAGE_PER_HR = 20;
+		public static final int DAY_IN_MONTH = 20;
+	
 	public static void main(String[] args) 
 	{
-		int EMP_IS_FULLTIME = 2;
-		int EMP_IS_PARTTIME = 1;
-		int EMP_WAGE_PER_HR = 20;
+		
 		int empHr = 0;
 		int empWage = 0;
+		int monthlyWage = 0;
 		Random random = new Random();
-		int empCheck = random.nextInt(3);
-		if (EMP_IS_FULLTIME == empCheck)
+		for(int day=1; day<=DAY_IN_MONTH; day++)
 		{
-			empHr = 8;
-			System.out.println("Employee is present.");
+			int empCheck = random.nextInt(3);
+			switch (empCheck)
+			{
+				case EMP_IS_FULLTIME:
+				{
+					empHr = 8;
+					break;
+				}
+				case EMP_IS_PARTTIME:
+				{
+					empHr = 4;
+					break;
+				}
+				default:
+					empHr = 0;
+			}
+			empWage = EMP_WAGE_PER_HR * empHr;
+			System.out.println("Employee daily wage is : " + empWage + " Rs");
+			monthlyWage += empWage;
 		}
-		else if (EMP_IS_PARTTIME == empCheck)
-		{
-			empHr = 4;
-			System.out.println("Employee is part time present");
-		}
-		empWage = EMP_WAGE_PER_HR * empHr;
-		System.out.println("Employee daily wage is : " + empWage + " Rs");
+		System.out.println("Employee monthly wage is : " + monthlyWage + " Rs");
 	}
 }
